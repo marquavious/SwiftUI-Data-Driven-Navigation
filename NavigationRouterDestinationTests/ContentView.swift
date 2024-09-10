cb30f97b9a73915f29b5f6e8bc9550287894197d
marquavious <marquavious@users.noreply.github.com>
2024-09-10 05:02:31 -0400

    Added Router Functionality

--- NavigationRouterDestinationTests/ContentView.swift
+++ NavigationRouterDestinationTests/ContentView.swift
@@ -9,53 +9,153 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+
+    @Environment(RouterPath.self) private var routerPath
+
+    @State var pageTitle: String
+    @State var pageDescription: String
+    @State var backgroundColor: Color
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+        ZStack {
+            backgroundColor
+            VStack(spacing: 18) {
+                VStack {
+                    Text("Page: \(pageTitle)")
+                    Text("Nice Color Pick. I like \(pageDescription), too!")
+
+                    Button {
+                         // Implment sheet
                     } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                        Text("Show Sheet")
+                    }
+                    .buttonStyle(.bordered)
+                }
+
+                HStack {
+                    ForEach(RouterDestination.allCases) { destination in
+                        Button {
+                            routerPath.navigate(to: destination)
+                        } label: {
+                            Text(destination.title)
+                                .foregroundStyle(.white)
+                                .font(.caption)
+                                .padding([.vertical], 2)
+                                .fontWeight(.bold)
+                        }
+                        .buttonStyle(.bordered)
+                        .background(destination.color)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .frame(height: 30)
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                Button {
+                    routerPath.popToRoot()
+                } label: {
+                    Text("Click Here To Pop To Root")
+                        .foregroundStyle(.white)
+                        .font(.caption)
+                        .padding([.vertical], 2)
+                        .fontWeight(.bold)
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+                .buttonStyle(.bordered)
+                .background(Color.pink)
+                .clipShape(RoundedRectangle(cornerRadius: 5))
+                .frame(height: 30)
             }
         }
     }
 }
 
 #Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+    Tab()
+}
+
+enum RouterDestination: Identifiable, Hashable, CaseIterable {
+    static var allCases = [Self.redPage,
+                           Self.bluePage,
+                           Self.yellowPage,
+                           Self.greenPage]
+
+    case redPage
+    case bluePage
+    case yellowPage
+    case greenPage
+//    case customPage(color: Color)
+
+    var id: String {
+        title
+    }
+
+    var title: String {
+        switch self {
+        case .redPage:
+            "Red Page"
+        case .bluePage:
+            "Blue Page"
+        case .yellowPage:
+            "Yello Page"
+        case .greenPage:
+            "Green Page"
+//        case .customPage(_):
+//            "Custom Color"
+        }
+    }
+
+    var color: Color {
+        switch self {
+        case .redPage:
+                .red
+        case .bluePage:
+                .blue
+        case .yellowPage:
+                .yellow
+        case .greenPage:
+                .green
+//        case .customPage(color: let color):
+//            color
+        }
+    }
+}
+
+@MainActor
+@Observable public class RouterPath {
+
+    var path: [RouterDestination] = []
+
+    nonisolated init() {}
+
+    func navigate(to: RouterDestination) {
+      path.append(to)
+    }
+
+    func popToRoot() {
+        path = []
+    }
+
+}
+
+@MainActor
+struct Tab: View {
+    @State var routerPath = RouterPath()
+
+    var body: some View {
+        NavigationStack(path: $routerPath.path) {
+            ContentView(
+                pageTitle: "Init", 
+                pageDescription: "Init Tab",
+                backgroundColor: .white
+            )
+            .withAppRouter()
+        }.environment(routerPath)
+    }
+}
+
+@MainActor
+extension View {
+    func withAppRouter() -> some View {
+        navigationDestination(for: RouterDestination.self) { destination in
+            createView(destination: destination)
+        }
+    }
 }
--- NavigationRouterDestinationTests/NavigationRouterDestinationTestsApp.swift
+++ NavigationRouterDestinationTests/NavigationRouterDestinationTestsApp.swift
@@ -7,26 +7,25 @@
 
 import SwiftUI
 import SwiftData
+import Observation
 
 @main
 struct NavigationRouterDestinationTestsApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Tab()
         }
-        .modelContainer(sharedModelContainer)
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func createView(destination: RouterDestination) -> some View {
+        ContentView(
+            pageTitle: destination.title, 
+            pageDescription: "Wow",
+            backgroundColor: destination.color
+        )
     }
 }
